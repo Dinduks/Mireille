68c22394e844f87f0e6bd18ef4ff6c54ec1c5b87
Samy Dindane <samy@dindane.com>
2013-03-03 23:39:44 +0100

    Display the info about a class passed in arguments

--- src/main/scala/com/Main.scala
+++ src/main/scala/com/Main.scala
@@ -1,8 +1,37 @@
-package main.scala.com
+package main.scala
+
+import com.dindane.mireille.models.InvokeVirtualCall
+import com.dindane.mireille.Reader
+import java.nio.file.Paths
 
 object Main {
 
   def main (args: Array[String]) {
+    if (1 == args.size) {
+      try {
+        val path = Paths.get(args(0))
+        val fileName = path.getFileName.toString
+
+        printVirtualCalls(fileName, Reader.getInvokeVirtualCalls(path))
+      } catch {
+        case e: Exception => {
+          println("The specified class file was not found.")
+          println("Exiting.")
+        }
+      }
+    }
+  }
+
+  def printVirtualCalls(fileName: String, invokeVirtualCalls: Seq[InvokeVirtualCall]) {
+    println("=== INVOKEVIRTUAL calls in '%s':".format(fileName))
+
+    invokeVirtualCalls.map { invokeVirtualCall =>
+      println()
+      println("Method name: %s".format(invokeVirtualCall.methodName))
+      println("Called on: %s".format(invokeVirtualCall.owner))
+      println("Takes these arguments: %s".format(invokeVirtualCall.arguments.mkString(", ")))
+      println("Returns: %s".format(invokeVirtualCall.returnType))
+    }
   }
 
 }
