2e7fa7e8266e3fea7a522fbba8db38d16e413d61
Samy Dindane <samy@dindane.com>
2013-04-12 00:31:54 +0200

    Replace INVOKEVIRTUAL by INVOKEDYNAMIC (still WIP)

--- src/main/scala/com/dindane/mireille/visitors/InvokeDynamicTransformerVisitor.scala
+++ src/main/scala/com/dindane/mireille/visitors/InvokeDynamicTransformerVisitor.scala
@@ -1,12 +1,29 @@
 package main.scala.com.dindane.mireille.visitors
 
-import org.objectweb.asm.{ClassWriter, MethodVisitor, ClassVisitor, Opcodes}
+import org.objectweb.asm._
 
-class InvokeDynamicTransformerVisitor(className: String, classWriter: ClassWriter)
-  extends ClassVisitor(Opcodes.ASM4, classWriter) {
-//  override def visitMethod(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]) = {
-//    new MethodVisitor(Opcodes.ASM4) {
-//
-//    }
-//  }
+class InvokeDynamicTransformerVisitor(className: String, classVisitor: ClassVisitor)
+  extends ClassVisitor(Opcodes.ASM4, classVisitor) {
+
+  var isStaticInitPatched: Boolean = false
+
+  override def visitMethod(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]) = {
+    val methodVisitor = classVisitor.visitMethod(access, name, desc, signature, exceptions)
+    new InvokeDynamicTransformerAdapter(methodVisitor)
+  }
+
+}
+
+class InvokeDynamicTransformerAdapter(methodVisitor: MethodVisitor)
+  extends MethodVisitor(Opcodes.ASM4, methodVisitor) {
+
+  override def visitMethodInsn(opcode: Int, owner: String, name: String, description: String) {
+    if (opcode == Opcodes.INVOKEVIRTUAL) {
+      val bsm = new Handle(Opcodes.H_INVOKESTATIC, owner, name, description)
+      super.visitInvokeDynamicInsn(name, description, bsm)
+    } else {
+      super.visitMethodInsn(opcode, owner, name, description)
+    }
+  }
+
 }
