a880f457dab0aa117ca49fee59d060e0a8c032d1
Samy Dindane <samy@dindane.com>
2013-04-16 01:02:14 +0200

    Rewrite Util.getPackageNameFromFQCN(String)

--- src/main/scala/com/dindane/mireille/Util.scala
+++ src/main/scala/com/dindane/mireille/Util.scala
@@ -3,6 +3,6 @@ package main.scala.com.dindane.mireille
 object Util {
 
   def getClassNameFromFQCN(fqcn: String): String = fqcn.split('/').last
-  def getPackageNameFromFQCN(fqcn: String): String = fqcn.split('/').take(fqcn.split('/').size - 1).mkString("/")
+  def getPackageNameFromFQCN(fqcn: String): String = fqcn.substring(0, fqcn.lastIndexOf("/"));
 
 }
