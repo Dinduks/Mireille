610e899522e9cca7adb18f2fe47d57c06692a666
Samy Dindane <samy@dindane.com>
2013-05-03 01:50:04 +0200

    WIP - Add a method to extract jars

---
+++ src/main/scala/com/dindane/mireille/jar/JarUtil.scala
@@ -0,0 +1,26 @@
+package main.scala.com.dindane.mireille.jar
+
+import java.util.jar.JarFile
+import java.io.{FileOutputStream, File}
+import collection.JavaConversions.enumerationAsScalaIterator
+
+object JarUtil {
+
+  def extract(jarPath: String, destinationDir: String) = {
+    val jar: JarFile = new JarFile(jarPath)
+
+    jar.entries.map { jarEntry =>
+      val file: File = new File(destinationDir + java.io.File.separator + jarEntry.getName)
+
+      if (jarEntry.isDirectory) file.mkdir
+
+      val is = jar.getInputStream(jarEntry)
+      val fos = new FileOutputStream(file)
+
+      while (is.available > 0) fos.write(is.read)
+
+      is.close
+    }
+  }
+
+}
