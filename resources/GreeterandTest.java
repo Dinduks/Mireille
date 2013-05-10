0600955a90229c1e29382c260b92f2175147534b
Samy Dindane <samy@dindane.com>
2013-05-10 23:17:10 +0200

    Update GreeterAndTest.java with a new bug

--- resources/GreeterandTest.java
+++ resources/GreeterandTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class TestOriginal {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
@@ -5,12 +7,21 @@ class TestOriginal {
         int[] ints = new int[42];
 
         // TODO: Fix this
-        // Exception in thread "main" java.lang.IllegalAccessException:
-        //     member is protected: [I.clone()Object, from TestOriginal
+//        Object: [I@2f57d162
+//        Method name: clone
+//        Exception in thread "main" java.lang.ClassCastException: required class TestOriginal but encountered class [I
+//        at java.lang.invoke.MethodHandleNatives.raiseException(MethodHandleNatives.java:374)
+//        at java.lang.invoke.MethodHandle.invokeWithArguments(MethodHandle.java:566)
+//        at main.scala.com.dindane.mireille.InliningCacheCallSite.fallback(RunTime.java:102)
+//        at TestOriginal.main(GreeterandTest.java:10)
         // int[] foo = ints.clone();
 
         // TODO: ASM verification of this call fails
         // greeter.toString();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.isEmpty();
+        list.contains("bla");
 
         System.out.println(greeter.inEnglish("Samy"));
         System.out.println(greeter.inFrenchPublic("Samy"));
